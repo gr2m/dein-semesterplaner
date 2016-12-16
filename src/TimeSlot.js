566b37253f025cb27f5347152f0eaf807ee48013
Lena Koester <lenakoester@Jarets-MBP.fritz.box>
2016-12-16 16:08:43 +0100

    added days and times as placeholders for actual course content

--- src/TimeSlot.js
+++ src/TimeSlot.js
@@ -3,7 +3,8 @@ import React from 'react';
 class TimeSlot extends React.Component {
   render() {
     return (
-      <div> hallo </div>
+      <div> {this.props.time}
+      </div>
     )
   }
 }
--- src/Weekday.js
+++ src/Weekday.js
@@ -2,9 +2,29 @@ import React from 'react';
 import TimeSlot from './TimeSlot';
 
 class Weekday extends React.Component {
+
+getTimeSlots() {
+    const sessions= [
+    { id:1, starttime:'8.00'},
+    { id:2, starttime:'9.45'},
+    { id:3, starttime:'12.15'},
+    { id:4, starttime:'14.00'},
+    { id:5, starttime:'15.45'},
+    ];
+    return sessions.map((slot) => {
+      return(
+        <TimeSlot  time={slot.starttime} key={slot.id} />
+        );
+    });
+  }
+
   render() {
+
+  	var timeslots= this.getTimeSlots();
     return (
-     <div> hallo </div>
+   		<div> {this.props.name} 
+   		<TimeSlot time={timeslots}/>
+   		</div>
     )
   }
 }
